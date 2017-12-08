e2090988341473b42603e799ee5a12e49084d5de
L kabeer <ahmed@Lkabeer.com>
2017-12-08 13:21:58 +0200

    version 1.2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = [];
 
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+
+  }
+
+  openTodoAlert() {
+    let addTodoAlert = this.alertCtrl.create({
+      title: "Add a Todo",
+      message: "Enter your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData) => {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todos.push(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
